31912e00c64cbbd62e45fcd7a8357228c31758af
Azat Khaliullin <azatkhaliullin@MacBook-Air-Azat.local>
2026-04-18 18:39:20 +0300

    mesto (отредактированный)

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -102,7 +102,7 @@ export const login = (
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      console.log(process.env.JWT_SECRET);
+      console.log(process.env);
       const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET as string, { expiresIn: '7d' });
       res.send({ token });
     })
